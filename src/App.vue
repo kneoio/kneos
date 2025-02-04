ede8bbadb410215aa57bb2d9cfd323ed91a6a1dc
kaira <justaidajam@gmail.com>
2025-02-04 20:40:49 +0000

    #

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
       <n-layout-header>
         <n-space align="center">
           <n-image width="40" height="40" src="/kneo.png" />
-          <h1>Kneo.io</h1>
+          <h1>Kneo</h1>
         </n-space>
       </n-layout-header>
       <n-layout-content>
         <n-card title="Reinventing Random Synergy">
-          Kneo.io claims to harness the power of interdimensional synergy
+          Kneo claims to harness the power of interdimensional synergy
           and revolutionize the synergy synergy-sphere with AI-driven hype.
         </n-card>
       </n-layout-content>
